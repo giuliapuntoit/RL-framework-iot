Add plot for requests and fix moving averages

plotter/plot_cdf_reward.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import fastplot
 import csv
 import numpy as np
 import pandas as pd
@@ -38,9 +39,25 @@ def compute_avg_reward_single_algo_multiple_runs(date_array, algorithm=None):
             next(reader, None)
             for row in reader:
                 x.append(int(row[0]))
-                y_avg_reward_for_one_episode.append(float(row[1]) / float(row[3]))
+                y_avg_reward_for_one_episode.append(float(row[1]))  # / float(row[3]))
         x_all.append(x)
         y_all_avg_rewards.append(y_avg_reward_for_one_episode)
+
+    data = []
+    for i in range(0, len(x_all)):
+        # plt.plot(episodes_target[i], avg_rew[i], label=algorithms_target[i], color=color[i])
+        # First sorting the array
+        plt.hist(np.sort(y_all_avg_rewards[i]), density=True, cumulative=True, label='CDF-run ' + str(i), histtype='step', alpha=0.8)
+        # data.append(("run"+str(i), y_all_avg_rewards[i]))
+    # fastplot.plot(data, 'CDF_PROVA.png', mode='CDF_multi', xlabel='Reward for algorithm ' + algorithm, legend=True,)
+
+    plt.xlabel('Reward')
+    plt.ylabel('CDF')
+    plt.legend(loc='lower right')
+    plt.title('CDF of avg reward in one episode')
+    plt.grid(True)
+    plt.savefig('cdf_rewards_PROVA.png')
+    plt.show()
 
     # compute average over multiple runs
     y_final_avg_rewards = []

plotter/plot_multiple_data.py
@@ -5,6 +5,7 @@ import numpy as np
 import fastplot
 from config import GlobalVar
 
+
 class PlotOutputData(object):
     # Plot reward and timesteps over episodes for multiple executions
     # Deprecated!
@@ -170,14 +171,14 @@ class PlotOutputData(object):
 
         data = pd.DataFrame(np.matrix(timesteps).transpose(),
                             index=episodes[0],
-                            columns=['0.3', '0.6', '0.9'],)
+                            columns=['0.3', '0.6', '0.9'], )
         fastplot.plot(data, GlobalVar.directory + 'plot/0_epsilon_bars_timesteps.png', mode='bars_multi', style='latex',
                       ylabel='Timesteps', legend=True, ylim=(0, 30), legend_ncol=3,
                       legend_args={'markerfirst': True})
 
         data = pd.DataFrame(np.matrix(rewards).transpose(),
                             index=episodes[0],
-                            columns=['0.3', '0.6', '0.9'],)
+                            columns=['0.3', '0.6', '0.9'], )
         fastplot.plot(data, GlobalVar.directory + 'plot/0_epsilon_bars_rew.png', mode='bars_multi', style='latex',
                       ylabel='Timesteps', legend=True, legend_ncol=3,
                       legend_args={'markerfirst': True})

plotter/plot_smoothed_avg.py
@@ -56,18 +56,18 @@ def plot_single_algo_single_run(date_to_retrieve):
 
     plt.show()
 
-    window_size = 30
+    window_size = 10
 
     # calculate the smoothed moving average
     weights = np.repeat(1.0, window_size) / window_size
-    yMA = np.convolve(y_reward, weights, 'full')
+    yMA = np.convolve(y_reward, weights, 'valid')
     # plot results
-    pl.plot(x, yMA[0:np.array(x).shape[0]], 'r', label='MA')
+    pl.plot(x[np.array(x).shape[0]-yMA.shape[0]:], yMA, 'r', label='MA')
     pl.plot(x, df['y1'], label='data')
     pl.xlabel('Time')
     pl.ylabel('y')
     pl.legend(loc='lower right')
-    pl.title('Moving Average with window size = 30')
+    pl.title('Moving Average with window size = ' + str(window_size))
     pl.grid(True)
     pl.show()
 
@@ -142,16 +142,17 @@ def plot_single_algo_multiple_runs(date_array, algorithm=None):
     # ["SARSA", "SARSA(λ)", "Q-learning", "Q(λ)"])
     color = ('#77FF82', '#47CC99', '#239DBA', '#006586')
 
-    window_size = 20
+    window_size = 10
 
     # calculate the smoothed moving average
     weights = np.repeat(1.0, window_size) / window_size
-    yMA = np.convolve(df_final_avg_over_n_runs['y1'], weights, 'full')
+    yMA = np.convolve(df_final_avg_over_n_runs['y1'], weights, 'valid')
+
     # plot results
     pl.plot(df_single_run['x'], df_single_run['y1'], label='single run', color=color[0])  # single line
     pl.plot(df_final_avg_over_n_runs['x'], df_final_avg_over_n_runs['y1'],
             label="avg over " + str(len(date_array)) + " run", color=color[1])  # avg line
-    pl.plot(df_final_avg_over_n_runs['x'], yMA[0:np.array(df_final_avg_over_n_runs['x']).shape[0]], 'r',
+    pl.plot(df_final_avg_over_n_runs['x'][np.array(df_final_avg_over_n_runs['x']).shape[0]-yMA.shape[0]:], yMA, 'r',
             label='moving average')  # moving avg line
 
     pl.xlabel('Episodes')
@@ -162,12 +163,13 @@ def plot_single_algo_multiple_runs(date_array, algorithm=None):
     plt.savefig('all_reward_plot.png')
     plt.show()
 
-    yMA_timesteps = np.convolve(df_final_avg_over_n_runs['y2'], weights, 'full')
+    yMA_timesteps = np.convolve(df_final_avg_over_n_runs['y2'], weights, 'valid')
+
     # plot results
     pl.plot(df_single_run['x'], df_single_run['y2'], label='single run', color=color[0])  # single line
     pl.plot(df_final_avg_over_n_runs['x'], df_final_avg_over_n_runs['y2'],
             label="avg over " + str(len(date_array)) + " run", color=color[1])  # avg line
-    pl.plot(df_final_avg_over_n_runs['x'], yMA_timesteps[0:np.array(df_final_avg_over_n_runs['x']).shape[0]], 'r',
+    pl.plot(df_final_avg_over_n_runs['x'][np.array(df_final_avg_over_n_runs['x']).shape[0]-yMA_timesteps.shape[0]:], yMA_timesteps, 'r',
             label='moving average')  # moving avg line
 
     pl.xlabel('Episodes')
@@ -186,7 +188,7 @@ def plot_multiple_algo_moving_avg(algorithms_target, episodes_target, moving_ave
     color = ('#77FF82', '#47CC99', '#239DBA', '#006586')
 
     for i in range(0, len(algorithms_target)):
-        pl.plot(episodes_target[i], moving_average_rewards_target[i][0:np.array(episodes_target[i]).shape[0]],
+        pl.plot(episodes_target[i][np.array(episodes_target[i]).shape[0]-np.array(moving_average_rewards_target[i]).shape[0]:], moving_average_rewards_target[i],
                 label=algorithms_target[i], color=color[i])
 
     pl.xlabel('Episodes')
@@ -198,7 +200,7 @@ def plot_multiple_algo_moving_avg(algorithms_target, episodes_target, moving_ave
     plt.show()
 
     for i in range(0, len(algorithms_target)):
-        pl.plot(episodes_target[i], moving_average_timesteps_target[i][0:np.array(episodes_target[i]).shape[0]],
+        pl.plot(episodes_target[i][np.array(episodes_target[i]).shape[0]-np.array(moving_average_timesteps_target[i]).shape[0]:], moving_average_timesteps_target[i],
                 label=algorithms_target[i], color=color[i])
 
     pl.xlabel('Episodes')

plotter/plot_smoothed_avg_for_params.py
@@ -72,12 +72,12 @@ def plot_single_configuration_multiple_runs(date_array, param):
 
     # color = ('#77FF82', '#47CC99', '#239DBA', '#006586')
 
-    window_size = 20
+    window_size = 10
 
     # Compute the smoothed moving average
     weights = np.repeat(1.0, window_size) / window_size
-    yMA = np.convolve(df_final_avg_over_n_runs['y1'], weights, 'full')
-    yMA_timesteps = np.convolve(df_final_avg_over_n_runs['y2'], weights, 'full')
+    yMA = np.convolve(df_final_avg_over_n_runs['y1'], weights, 'valid')
+    yMA_timesteps = np.convolve(df_final_avg_over_n_runs['y2'], weights, 'valid')
 
     # Compute also the global avg reward and the number of rewards >= average in percentage
     # Compute also the global avg timesteps and the number of rewards <= average in percentage
@@ -130,7 +130,7 @@ def plot_multiple_configuration_moving_avg(algorithm, param, param_values_target
     color = ('#A66066', '#F2A172', '#858C4A')
 
     for i in range(0, len(param_values_target)):
-        pl.plot(episodes_target[i], moving_average_rewards_target[i][0:np.array(episodes_target[i]).shape[0]],
+        pl.plot(episodes_target[i][np.array(episodes_target[i]).shape[0]-np.array(moving_average_rewards_target[i]).shape[0]:], moving_average_rewards_target[i],
                 label=param + "=" + param_values_target[i], color=color[i])
 
     pl.xlabel('Episodes')
@@ -142,7 +142,7 @@ def plot_multiple_configuration_moving_avg(algorithm, param, param_values_target
     plt.show()
 
     for i in range(0, len(param_values_target)):
-        pl.plot(episodes_target[i], moving_average_timesteps_target[i][0:np.array(episodes_target[i]).shape[0]],
+        pl.plot(episodes_target[i][np.array(episodes_target[i]).shape[0]-np.array(moving_average_timesteps_target[i]).shape[0]:], moving_average_timesteps_target[i],
                 label=param + "=" + param_values_target[i], color=color[i])
 
     pl.xlabel('Episodes')