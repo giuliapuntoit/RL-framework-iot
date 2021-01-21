7de2e31d6108c2992115e21fad86008b0fadbc47
Giulia Milan <giuliapuntoit96@gmail.com>
2021-01-21 14:23:42 +0100

    Fix plots

--- plotter/support_plotter.py
+++ plotter/support_plotter.py
@@ -7,6 +7,22 @@ from matplotlib import patches
 from config import FrameworkConfiguration
 
 
+def print_cute_algo_name(a):
+    """
+    Return algorithm with greek letters
+    """
+    if a == "sarsa":
+        return "SARSA"
+    elif a == "sarsa_lambda":
+        return "SARSA(λ)"
+    elif a == "qlearning":
+        return "Q-learning"
+    elif a == "qlearning_lambda":
+        return "Q(λ)"
+    else:
+        return "invalid"
+
+
 def fix_hist_step_vertical_line_at_end(ax):
     """
     Support function to adjust layout of plots
@@ -17,6 +33,9 @@ def fix_hist_step_vertical_line_at_end(ax):
 
 
 def build_directory_and_filename(algorithm, date):
+    """
+    Find directory and the filename to retrieve data
+    """
     if algorithm is None:
         directory = FrameworkConfiguration.directory + 'output/output_Q_parameters'
         file_parameters = 'output_parameters_' + date + '.csv'
@@ -26,6 +45,7 @@ def build_directory_and_filename(algorithm, date):
             parameters = {rows[0].strip(): rows[1].strip() for rows in reader}
 
         algorithm = parameters['algorithm_used']
+
     print("RL ALGORITHM:", algorithm)
 
     directory = FrameworkConfiguration.directory + 'output/output_csv'
@@ -34,10 +54,16 @@ def build_directory_and_filename(algorithm, date):
     return directory, filename
 
 
-def read_avg_reward_from_output_file(algorithm, dat):
-    directory, filename = build_directory_and_filename(algorithm, dat)
+def read_avg_reward_from_output_file(algorithm, date_to_retrieve):
+    """
+    Retrieve and compute the average reward per time step for episodes from output
+    """
+
+    directory, filename = build_directory_and_filename(algorithm, date_to_retrieve)
+
     x = []
     y_avg_reward_for_one_episode = []
+
     with open(directory + '/' + filename, 'r') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         next(reader, None)
@@ -48,3 +74,50 @@ def read_avg_reward_from_output_file(algorithm, dat):
             y_avg_reward_for_one_episode.append(float(row[1]) / float(row[3]))
 
     return x, y_avg_reward_for_one_episode
+
+
+def read_reward_timesteps_from_output_file(algorithm, date_to_retrieve):
+    """
+    Read reward, cumulative reward and timesteps data from output file
+    """
+    directory, filename = build_directory_and_filename(algorithm, date_to_retrieve)
+
+    x = []
+    y_reward = []
+    y_cum_reward = []
+    y_timesteps = []
+
+    with open(directory + '/' + filename, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        next(reader, None)
+        for row in reader:
+            x.append(int(row[0]))
+            y_reward.append(int(row[1]))
+            y_cum_reward.append(int(row[2]))
+            y_timesteps.append(int(row[3]))
+
+    return x, y_reward, y_cum_reward, y_timesteps
+
+
+def compute_avg_over_multiple_runs(number_episodes, number_runs, y_all_reward, y_all_cum_reward, y_all_timesteps):
+    """
+    Compute average of reward and timesteps over multiple runs (different dates)
+    """
+    y_final_reward = []
+    y_final_cum_reward = []
+    y_final_timesteps = []
+    for array_index in range(0, number_episodes):
+        sum_r = 0
+        sum_cr = 0
+        sum_t = 0
+        count = 0
+        for date_index in range(0, number_runs):  # compute average
+            sum_r += y_all_reward[date_index][array_index]
+            sum_cr += y_all_cum_reward[date_index][array_index]
+            sum_t += y_all_timesteps[date_index][array_index]
+            count += 1
+        y_final_reward.append(sum_r / float(count))
+        y_final_cum_reward.append(sum_cr / float(count))
+        y_final_timesteps.append(sum_t / float(count))
+
+    return y_final_reward, y_final_cum_reward, y_final_timesteps
